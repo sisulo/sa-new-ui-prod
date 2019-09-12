791cf10d2fd57a78b90a755b9874d35fe79845f1
Ivan Sisak <ivo.sisak@gmail.com>
2019-09-12 23:16:26 +0200

    New version

--- main.js
+++ main.js
@@ -1336,41 +1336,6 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 var __metadata = (undefined && undefined.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 
 
 
@@ -1395,38 +1360,31 @@ var RowGroupTableComponent = /** @class */ (function () {
         this.alertPriority = ['text-alert-yellow', 'text-orange', 'text-red'];
     }
     RowGroupTableComponent.prototype.ngOnInit = function () {
-        return __awaiter(this, void 0, void 0, function () {
-            var _a;
-            var _this = this;
-            return __generator(this, function (_b) {
-                switch (_b.label) {
-                    case 0:
-                        // TODO refactor this to the service
-                        this.localStorageService.observe(this.options.storageNamePrefix + '_collapsed').subscribe(function (data) {
-                            _this.collapsedRows = data.newValue;
-                        });
-                        _a = this;
-                        return [4 /*yield*/, this.localStorageService.get(this.options.storageNamePrefix + '_collapsed')];
-                    case 1:
-                        _a.collapsedRows = _b.sent();
-                        if (this.selectedRows == null) {
-                            this.selectedRows = [];
-                        }
-                        else {
-                            this.selectedRows = this.selectedRows; // this must be reset because save on the collapsedRows doesn't work
-                        }
-                        if (this.collapsedRows === null) {
-                            this.collapsedRows = [];
-                        }
-                        else {
-                            this.collapsedRows = this.collapsedRows; // this must be reset because save on the collapsedRows doesn't work
-                        }
-                        this.initAggregatedValues();
-                        this.summarizeAlerts();
-                        return [2 /*return*/];
-                }
-            });
+        var _this = this;
+        // TODO refactor this to the service
+        this.localStorageService.observe(this.options.storageNamePrefix + '_selected').subscribe(function (data) {
+            _this.selectedRows = data.newValue;
+            _this.initAggregatedValues();
         });
+        this.localStorageService.observe(this.options.storageNamePrefix + '_collapsed').subscribe(function (data) {
+            _this.collapsedRows = data.newValue;
+        });
+        this.selectedRows = this.localStorageService.get(this.options.storageNamePrefix + '_selected');
+        this.collapsedRows = this.localStorageService.get(this.options.storageNamePrefix + '_collapsed');
+        if (this.selectedRows == null) {
+            this.selectedRows = [];
+        }
+        else {
+            this.selectedRows = this.selectedRows; // this must be reset because save on the collapsedRows doesn't work
+        }
+        if (this.collapsedRows === null) {
+            this.collapsedRows = [];
+        }
+        else {
+            this.collapsedRows = this.collapsedRows; // this must be reset because save on the collapsedRows doesn't work
+        }
+        this.initAggregatedValues();
+        this.summarizeAlerts();
     };
     RowGroupTableComponent.prototype.initAggregatedValues = function () {
         if (this.selectedRows === null) {
@@ -1476,7 +1434,10 @@ var RowGroupTableComponent = /** @class */ (function () {
         var values = mean.computeSummaries([this.data], selectedRows, this.options);
         var result = {};
         if (values !== null) {
-            this.options.getAggregatedColumns().forEach(function (column) { return result[column.index] = { value: values.getValue(column.index).value, unit: values.getValue(column.index).unit }; });
+            this.options.getAggregatedColumns().forEach(function (column) { return result[column.index] = {
+                value: values.getValue(column.index).value,
+                unit: values.getValue(column.index).unit
+            }; });
         }
         this.aggregatedValues = result;
     };
@@ -1581,41 +1542,6 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 var __metadata = (undefined && undefined.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 
 
 
@@ -1635,24 +1561,12 @@ var RowTableComponent = /** @class */ (function () {
         }
     };
     RowTableComponent.prototype.ngOnInit = function () {
-        return __awaiter(this, void 0, void 0, function () {
-            var _a;
-            var _this = this;
-            return __generator(this, function (_b) {
-                switch (_b.label) {
-                    case 0:
-                        this.subscription = this.onSelectService.selectAll$.subscribe(function (value) { return _this.selectRow(_this.data.getCell('name').value, value); });
-                        _a = this;
-                        return [4 /*yield*/, this.localStorageService.get(this.options.storageNamePrefix + '_selected')];
-                    case 1:
-                        _a.selectedRows = _b.sent();
-                        if (this.selectedRows === null) {
-                            this.selectedRows = [];
-                        }
-                        return [2 /*return*/];
-                }
-            });
-        });
+        var _this = this;
+        this.subscription = this.onSelectService.selectAll$.subscribe(function (value) { return _this.selectRow(_this.data.getCell('name').value, value); });
+        this.selectedRows = this.localStorageService.get(this.options.storageNamePrefix + '_selected');
+        if (this.selectedRows === null) {
+            this.selectedRows = [];
+        }
     };
     /* HIGHLIGHTNING */
     RowTableComponent.prototype.isColumnHighlighted = function (column) {
